8d360405d862ac6287bb9a3798af226dd167f62a
saraph1nes <xiayx.leo@qq.com>
2020-10-29 21:45:22 +0800

    first push

--- src/main/java/cn/lynu/util/Utils.java
+++ src/main/java/cn/lynu/util/Utils.java
@@ -1,11 +1,13 @@
 package cn.lynu.util;
 
+
+import org.springframework.util.DigestUtils;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.net.URLEncoder;
+import java.util.Base64;
 import java.util.UUID;
-import javax.servlet.http.HttpServletRequest;
-import org.springframework.util.DigestUtils;
-import sun.misc.BASE64Encoder;
+import java.util.Base64.Encoder;
 
 public class Utils {
 	
@@ -19,16 +21,19 @@ public class Utils {
 	
 	//用来对下载文件名进行编码
     public static String filenameEncoding(String filename, HttpServletRequest request) throws IOException {
-        String agent = request.getHeader("User-Agent"); //获取浏览器
-        if (agent.contains("Firefox")) {
-            BASE64Encoder base64Encoder = new BASE64Encoder();
-            filename = "=?utf-8?B?"
-                    + base64Encoder.encode(filename.getBytes("utf-8"))
-                    + "?=";
-        }else {
-            filename = URLEncoder.encode(filename, "utf-8");
-        }
-        return filename;
+//        String agent = request.getHeader("User-Agent"); //获取浏览器
+//        if (agent.contains("Firefox")) {
+//            //BASE64Encoder base64Encoder = new BASE64Encoder();
+//            Encoder encoder = Base64.getEncoder();
+//            String result = encoder.encodeToString(byteArray);
+//            filename = "=?utf-8?B?"
+//                    + base64Encoder.encode(filename.getBytes("utf-8"))
+//                    + "?=";
+//        }else {
+//            filename = URLEncoder.encode(filename, "utf-8");
+//        }
+//        return filename;
+		return "";
     }
 
 }
